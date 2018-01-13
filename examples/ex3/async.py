cc90b7a677b7ae3fe559397761a7af48493a51e4
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2018-01-12 18:50:32 -0800

    Travis fix + Python3 for async.py

--- examples/ex3/async.py
+++ examples/ex3/async.py
@@ -7,6 +7,7 @@ import async
 import pythreading
 
 waitForFutures = False
+pyfib = 27
 
 pythreading.thread_manager(4)
 
@@ -18,28 +19,30 @@ def fibonacci(n):
 c_start = time.clock()
 w_start = time.time()
 
-async.write("(Python) Starting loop calling C++ function scheduling asynchronous calculations...")
+async.write("(Python) Starting loop calling C++ function scheduling "
+            "asynchronous calculations...")
 
 futures = []
-for i in range(0, 50):
-    async.write("(Python) Running async iteration %i..." % i)
-    futures.append(async.run())
+for i in range(0, 100):
+    async.write("(Python) Running async iteration {}...".format(i))
+    if i < 50:
+        futures.append(async.run())
+    else:
+        futures.append(async.work(i))
 
-for i in range(50, 100):
-    async.write("(Python) Running async iteration %i..." % i)
-    futures.append(async.work(i))
-
-async.write("(Python) Finished any calls to C++ code... Any future C++ output demonstrates C++ running in the background alongside Python...")
+async.write("(Python) Finished any calls to C++ code... Any future C++ output "
+            "demonstrates C++ running in the background alongside Python...")
 
 if waitForFutures == True:
     for n in range(0, len(futures)):
+        async.write("(Python) Waiting on future {}...".format(n))
         futures[n].wait()
 
 sum = 0
 for n in range(0, len(futures)):
-    async.write("(Python) Getting future for %i..." % n)
     # do work in Python to try to exceed the ~400% utilization of C++
-    sum += fibonacci(27)
+    sum += fibonacci(pyfib)
+    async.write("(Python) Getting future for {}...".format(n))
     sum += futures[n].get()
 
 c_end = time.clock()
@@ -50,12 +53,12 @@ w_diff = w_end - w_start
 cpu_usage = (c_diff / w_diff) * 100.
 
 async.write("")
-async.write("(Python) Sum = %i" % sum)
+async.write("(Python) Sum = {}".format(sum))
 async.write("")
-async.write("CPU time : %f seconds" % c_diff)
-async.write("Wall time : %f seconds" % w_diff)
+async.write("CPU time : {} seconds".format(c_diff))
+async.write("Wall time : {} seconds".format(w_diff))
 async.write("")
-async.write("%s CPU : %6.2f%s" % ('%', cpu_usage, '%'))
+async.write("% CPU : {:6.2f}%".format(cpu_usage))
 
 ans = (sum == 10253057300)
 if ans == True:
